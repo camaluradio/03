41dbe5f1da2db25393f786bf3df1b6f41df9cf26
Camalú Radio <camaluradio@gmail.com>
2025-10-19 16:32:05 -0300

    Update and rename js_sharepost.js to js_shares.js

--- js_shares.js
+++ js_shares.js
@@ -6,11 +6,11 @@
   const url = encodeURIComponent(window.location.href);
   const title = encodeURIComponent(document.title);
   document.getElementById('shareFacebook').href = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
-  document.getElementById('shareTwitter').href = `https://twitter.com/intent/tweet?url=${url}&amp;text=${title}`;
+  document.getElementById('shareTwitter').href = `https://twitter.com/intent/tweet?url=${url}`;
   document.getElementById('shareWhatsApp').href = `https://api.whatsapp.com/send?text={url}`;
   document.getElementById('shareLinkedIn').href = `https://www.linkedin.com/sharing/share-offsite/?url=${url}`;
-  document.getElementById('shareTelegram').href = `https://t.me/share/url?url=${url}&amp;text=${title}`;
-  document.getElementById('shareDiscord').href = `https://discord.com/channels/@me?text=${title}%20${url}`;
+  document.getElementById('shareTelegram').href = `https://t.me/share/url?url=${url}`;
+  document.getElementById('shareDiscord').href = `https://discord.com/channels/@me?text={url}`;
   document.getElementById('copyLink').addEventListener('click', function(){
     navigator.clipboard.writeText(window.location.href).then(() => {
       alert('¡Enlace copiado al portapapeles!');
